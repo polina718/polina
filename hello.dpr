309d610dbd410f63377e4caf5d781a4159bd4702
polina.shakrieva@mail.ru <polina718>
2020-01-27 09:59:47 +0500

    commit

---
+++ hello.dpr
@@ -0,0 +1,14 @@
+program hello;
+
+uses
+  Vcl.Forms,
+  world in 'world.pas' {Form3};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm3, Form3);
+  Application.Run;
+end.
